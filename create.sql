b7ad3fa295323a797bf30cbdc54cc311a4257916
Djordje Przulj <djordje.przulj@devoteam.com>
2024-04-24 20:40:21 +0200

    Ide files

--- create.sql
+++ create.sql
@@ -10,3 +10,5 @@ create sequence notification_sequence start with 1 increment by 1;
 create table notification_request (priority integer, id bigint not null, channel varchar(255) not null, destinations varchar(255) not null, source varchar(255) not null, text varchar(255), primary key (id));
 create sequence notification_sequence start with 1 increment by 1;
 create table notification_request (priority integer, id bigint not null, channel varchar(255) not null, destinations varchar(255) not null, source varchar(255) not null, text varchar(255), primary key (id));
+create sequence notification_sequence start with 1 increment by 1;
+create table notification_request (priority integer, id bigint not null, channel varchar(255) not null, destinations varchar(255) not null, source varchar(255) not null, text varchar(255), primary key (id));
